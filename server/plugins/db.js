1b16c6804f80ef4caa555718218dbadb5ee0acfb
ltyy <1129586942@qq.com>
2020-03-28 14:36:53 +0800

    feature:转移设备，安装依赖

--- server/plugins/db.js
+++ server/plugins/db.js
@@ -2,7 +2,8 @@
 module.exports = app => {
   const mongoose = require("mongoose")
   mongoose.connect('mongodb://127.0.0.1:27017/node-vue-moba', {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
   })
 
   // 把 models 文件夹的所有文件都引用进来，使用一遍
